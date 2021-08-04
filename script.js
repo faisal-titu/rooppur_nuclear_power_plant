e5680db4f3da9ebdac0514f4345a393034cd0eff
Faisal Ahmed <71240981+faisal-titu@users.noreply.github.com>
2021-08-04 21:59:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,159 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink = document.querySelectorAll('.nav__link')
+
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+//chage background header
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+
+window.addEventListener('scroll', scrollHeader)
+
+
+
+// var swiper = new Swiper(".discover__container", {
+//     effect: "coverflow",
+//     grabCursor: true,
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     loop: true,
+//     spaceBetween: 20,
+//     coverflowEffect: {
+//       rotate: 50,
+      
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+// });
+
+//video
+
+// const videoFile = document.getElementById('video-file'),
+//       videoButton = document.getElementById('video-button'),
+//       videoIcon = document.getElementById('video-icon')
+
+// function playPause(){
+//     if(videoFile.paused){ 
+//         videoFile.play();
+
+//         videoIcon.classList.add('ri-pause-line')
+//         videoIcon.classList.remove('ri-play-line')
+//     }else{
+//         videoFile.pause()
+
+//         videoIcon.classList.remove('ri-pause-line')
+//         videoIcon.classList.add('ri-play-line')
+
+//     }
+// }
+  
+// videoButton.addEventListener('click', playPause)
+
+// function finalVideo(){
+//     videoIcon.classList.remove('ri-pause-line')
+//     videoIcon.classList.add('ri-play-line')
+// }
+
+// videoFile.addEventListener('ended', finalVideo)
+
+
+// scroll Up
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+// scroll section active link
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+
+// // scroll animation 
+// const sr = ScrollReveal({
+//     distance: '60px',
+//     duration: 2400,
+// })
+
+// sr.reveal(`.home__data, .home__social-link, .home__info`,{
+//     origin: 'top',
+//     interval: 100,
+// })
+
+
+
+// photo slider
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
